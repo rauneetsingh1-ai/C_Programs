eb234238e37ef773a3812783998cfedf182ffd7c
Rauneet Singh <rauneetsingh1@gmail.com>
2025-10-31 05:05:11 +0530

    Add files via upload

---
+++ update_array_element.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {2, 4, 8, 12, 16};
+
+    // Update the first value of the array
+    arr[0] = 1;
+    printf("%d\n", arr[0]);
+    return 0;
+}
